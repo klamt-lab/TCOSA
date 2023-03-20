36221e2ca22577c32fbdf45ab8a778adb95f5438
Paulocracy <36934614+Paulocracy@users.noreply.github.com>
2023-03-20 10:54:20 +0100

    Full run fix

--- cosa_dG0_sampling_aerobic.py
+++ cosa_dG0_sampling_aerobic.py
@@ -2,4 +2,4 @@ from cosa_dG0_sampling import cosa_dG0_sampling
 from cosa_random_sampling_figures import create_cosa_dG0_sampling_figures, create_total_dG0_sampling_figure
 
 cosa_dG0_sampling(anaerobic=False, expanded=False, num_samplings=100, change_range=5)
-create_total_dG0_sampling_figure(change_range=5)
+# create_total_dG0_sampling_figure(change_range=5)
