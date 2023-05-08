f38cb98efe9691404c1d21463d592e084ed284ae
Paulocracy <36934614+Paulocracy@users.noreply.github.com>
2023-05-08 13:39:00 +0200

    Prepare in vivo single-swap analysis

--- cosa_single_swap_effect_analysis.py
+++ cosa_single_swap_effect_analysis.py
@@ -8,13 +8,11 @@ from cosa_get_all_tcosa_reaction_ids import get_all_tcosa_reaction_ids
 from cosa_get_suffix import cosa_get_suffix
 from fba import get_fba_base_problem, perform_fba_flux_maximization
 from helper import ensure_folder_existence, json_load, json_write, json_zip_load, json_zip_write
-from optmdfpathway import STANDARD_R, STANDARD_T, get_optmdfpathway_base_problem, get_thermodynamic_bottlenecks
+from optmdfpathway import STANDARD_R, STANDARD_T, get_optmdfpathway_base_problem
 from optimization import perform_variable_maximization
-from cosa_create_table import create_cosa_tables
 from cosa_load_model_data import (
     MIN_OPTMDF, load_model_data
 )
-from cosa_random_sampling_figures import create_cosa_figures, create_total_cosa_figure
 from cosa_get_model_with_nadx_scenario import cosa_get_model_with_nadx_scenario
 from cosa_add_promiscuity_constraints import cosa_add_promiscuity_constraints
 
@@ -30,7 +28,7 @@ def cosa_single_swap_analysis(anaerobic: bool):
     nadx_scenario = "WILDTYPE"
     old_cobra_model = copy.deepcopy(cobra_model)
     biomass_reaction_id = "BIOMASS_Ec_iML1515_core_75p37M"
-    for concentration_scenario in ("STANDARDCONC",): # "VIVOCONC"):
+    for concentration_scenario in ("VIVOCONC",): # "STANDARDCONC"):
         swap_json_path = f"./cosa/results{suffix}/swap_results_{concentration_scenario}.json"
 
         if concentration_scenario == "STANDARDCONC":
@@ -83,10 +81,10 @@ def cosa_single_swap_analysis(anaerobic: bool):
             swap_results = {}
         counter = 0
         for reaction in cobra_model.reactions:
-            if (reaction.id) not in dG0_values.keys():
+            if (reaction.id not in dG0_values.keys()):
                 continue
 
-            if (reaction.id not in swap_results.keys()):
+            if (reaction.id in swap_results.keys()):
                 continue
 
             if reaction.id.endswith("_ORIGINAL_NAD_TCOSA"):
