70a5ab9c5b1c5d53feb107e3f5bcb686977007c9
Paulocracy <36934614+Paulocracy@users.noreply.github.com>
2023-05-16 14:10:42 +0200

    Prepare anaerobic expanded run

--- cosa_random_sampling_anaerobic.py
+++ cosa_random_sampling_anaerobic.py
@@ -1,6 +1,7 @@
 from cosa_random_sampling import cosa_random_sampling
 
-cosa_random_sampling(anaerobic=True, expanded=False, num_randoms_random=500, num_randomfixed_random=500)
+# Temporary comment
+# cosa_random_sampling(anaerobic=True, expanded=False, num_randoms_random=500, num_randomfixed_random=500)
 cosa_random_sampling(anaerobic=True, expanded=True, num_randoms_random=1, num_randomfixed_random=1)
 cosa_random_sampling(anaerobic=True, expanded=True, num_randoms_random=1, num_randomfixed_random=1, fixed_nadx_change=+30)
 cosa_random_sampling(anaerobic=True, expanded=True, num_randoms_random=1, num_randomfixed_random=1, fixed_nadx_change=-30)
